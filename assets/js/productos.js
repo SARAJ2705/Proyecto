c7ebfcaf02ad795bfa1f70b5d2ac9e098085d62e
MiunoU145 <jeancamilocortina@gmail.com>
2024-11-29 14:54:22 -0500

    productos

---
+++ assets/js/productos.js
@@ -0,0 +1,27 @@
+//DOM
+document.addEventListener('DOMContentLoaded', () =>{ 
+    //Variables
+    const baseDeDatos = [
+        {
+            id: 1,
+            nombre: 'Postres de 3 leches fresa',
+            precio: 3500,
+            imagen: 'assets/img/postre_fresa.jpeg',
+            categoria: 'Postres 3 leches'
+        },
+
+
+
+
+
+
+
+    ];
+
+
+
+
+
+}
+
+);
